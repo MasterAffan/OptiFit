c0503adc51dbec9c85e6b950e2e49c436ac8ab0c
MasterAffan <masteraffan@gmail.com>
2025-08-19 23:21:47 +0530

    flutter analyze error fixed

--- optifit app/test/widget_test.dart
+++ optifit app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gymbuddy/main.dart';
+import 'package:OptiFit/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
